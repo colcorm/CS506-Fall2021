a1268f8a4eb6c78658ecc8641f4279254597050e
Colin Cormier <colcorm@csa1.bu.edu>
2021-09-21 12:54:54 -0400

    fixed errors in impute_missing and find_medians, created homework-0.txt to place written answers, and implemented the discard_missing function

--- homework-0-colcorm-answers/homework-0.py
+++ homework-0-colcorm-answers/homework-0.py
@@ -27,37 +27,57 @@ def impute_missing(at_arry):
     medians = find_medians(at_arry)
     for line in at_arry:
         attr_cat = 0
-        for attr in at_arry:
+        for attr in line:
+            if attr != attr:
+                line[attr_cat] = medians[attr_cat]
             attr_cat += 1
-            if attr == float("nan"):
-                attr = medians[attr_cat]
     return at_arry
 
 # Purpose: Finds the median values of each category of attributes
 def find_medians(at_arry):
     # Takes as input an array
     # Returns an array of the median values of each attribute
-    medians, temp = [], []
-    
+    medians = []
     medians = [[row[i] for row in at_arry] for i in range(len(at_arry[0]))]
 
+    count = 0
     for line in medians:
-        for attr in line:
-            if attr == float("nan"):
-                line.remove(float("nan"))
-    for line in medians:
+        line = [attr for attr in line if attr == attr]
         line = sorted(line)
         len_line = len(line)
         index = (len_line - 1) // 2
 
         if (len_line % 2):
-            line = line[index]
+            medians[count] = line[index]
         else:
-            line = (line[index] + line[index + 1])/2.0
-    
+            medians[count] = (line[index] + line[index + 1])/2.0
+        count += 1
     return medians
 
+# Purpose: Discards samples tht do not have an entry for every attribute
+def discard_missing(at_arry, cl_arry):
+    # Takes as input two arrays
+    # Returns both arrays with missing entries discarded from both
+    delete = []
+    x_count = 0
+    del_count = 0
+    for x in at_arry:
+        for y in x:
+            if y != y:
+                at_arry[x_count] = -1
+                cl_arry[x_count] = -1
+                del_count += 1
+        x_count += 1
     
+    for x in range(0, del_count):
+        at_arry.remove(-1)
+        cl_arry.remove(-1)
+        
+    
+    return at_arry
+
+
+
     
             
             
